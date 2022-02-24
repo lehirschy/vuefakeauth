4cbbb3988cec17e0dc04d52c1fe4122be5c812f8
lehirschy <edensjunk@yahoo.com>
2022-02-24 11:58:15 -0600

    Local Changes

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,21 +1,21 @@
 <template>
-    <div class="text-blue-200 bg-purple-800">
+    <div class="text-green-200 bg-green-800">
         <div class="container flex items-center justify-between mx-auto " >
-            <h1 class="text-3xl font-thin tracking-tighter">Basic<span class="font-normal">Router</span></h1>
+            <h1 class="text-3xl font-thin tracking-tighter">Vue<span class="font-normal">FakeAuth</span></h1>
             <nav>
                 <ul class="flex w-full space-x-4">
                     <router-link to="/">
-                        <li class="px-4 py-8 hover:cursor-pointer hover:bg-purple-100 hover:text-purple-800">
+                        <li class="px-4 py-8 hover:cursor-pointer hover:bg-blue-100 hover:text-blue-800">
                             Home 
                         </li>
                     </router-link>
                     <router-link to="/about">
-                        <li class="px-4 py-8 hover:cursor-pointer hover:bg-purple-100 hover:text-purple-800">
+                        <li class="px-4 py-8 hover:cursor-pointer hover:bg-blue-100 hover:text-blue-800">
                             About
                         </li>
                     </router-link>
                     <router-link to="/hello">
-                        <li class="px-4 py-8 hover:cursor-pointer hover:bg-purple-100 hover:text-purple-800">
+                        <li class="px-4 py-8 hover:cursor-pointer hover:bg-blue-100 hover:text-blue-800">
                             Hello
                         </li>
                     </router-link>
