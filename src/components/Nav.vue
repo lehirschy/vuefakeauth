10b08e4c4c782ec3038185e650a1851771a403bc
lehirschy <98000073+lehirschy@users.noreply.github.com>
2022-02-24 11:44:06 -0600

    Initial commit

---
+++ src/components/Nav.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="text-blue-200 bg-purple-800">
+        <div class="container flex items-center justify-between mx-auto " >
+            <h1 class="text-3xl font-thin tracking-tighter">Basic<span class="font-normal">Router</span></h1>
+            <nav>
+                <ul class="flex w-full space-x-4">
+                    <router-link to="/">
+                        <li class="px-4 py-8 hover:cursor-pointer hover:bg-purple-100 hover:text-purple-800">
+                            Home 
+                        </li>
+                    </router-link>
+                    <router-link to="/about">
+                        <li class="px-4 py-8 hover:cursor-pointer hover:bg-purple-100 hover:text-purple-800">
+                            About
+                        </li>
+                    </router-link>
+                    <router-link to="/hello">
+                        <li class="px-4 py-8 hover:cursor-pointer hover:bg-purple-100 hover:text-purple-800">
+                            Hello
+                        </li>
+                    </router-link>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</template>
